4f38bf88c36358b41e369a18d70607a20ab6e264
mschmitz42 <119008661+mschmitz42@users.noreply.github.com>
2022-12-14 18:54:25 -0800

    Add core project

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,5 +4,6 @@ from tracker import views
 app_name = 'tracker'
 
 urlpatterns = [
+    # path('', views.tracker_index, name='tracker_index'),
     path('', views.tracker_index, name='tracker_index'),
 ]
