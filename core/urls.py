5db20ee98013e8135bd3c14ff02352bfdda9997f
mschmitz42 <119008661+mschmitz42@users.noreply.github.com>
2022-12-26 18:52:15 -0800

    Made several Styling improvements

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.core_index, name='core_index'),
+    path('about', views.core_about, name='core_about'),
+    path('scheduler', views.core_scheduler, name='core_scheduler'),
+    path('profile', views.core_profile, name='core_profile'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def core_index(request):
     return render(request, 'core/index.html')
 
+
+def core_about(request):
+    return render(request, 'core/about.html')
+
+
+def core_scheduler(request):
+    return render(request, 'core/under-construction.html')
+
+
+def core_profile(request):
+    return render(request, 'core/under-construction.html')
